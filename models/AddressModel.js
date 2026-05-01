8fc4d1fbe78c8470092c52d0bb706417a42f1977
kiranbabup <kiran12146@gmail.com>
2026-05-01 22:39:45 +0530

    error update

--- models/AddressModel.js
+++ models/AddressModel.js
@@ -1,6 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
-import User from "./UserModel.js";
+import User from "./User.js";
 
 const Address = sequelize.define(
   "Address",
