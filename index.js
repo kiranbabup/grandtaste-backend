fb53ed1e6e071a6e45619277b3fcec148b9d656d
kiranbabup <kiran12146@gmail.com>
2026-05-01 22:49:45 +0530

    found a bug cleared in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import orderRoutes from "./routes/orderRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import wishlistRoutes from "./routes/wishlistRoutes.js";
 // import uploadRoutes from "./routes/uploadRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
+// import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -61,7 +61,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 // app.use("/api/upload", uploadRoutes);
-app.use("/api/admin", adminRoutes);
+// app.use("/api/admin", adminRoutes);
 // ==================== END ROUTES ====================
 
 app.get("/", (req, res) => {
